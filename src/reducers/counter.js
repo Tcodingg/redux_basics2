26023340076447d0ede4ab46d5fe36b4d67e4204
DePrincio <deprincio@DePrincio.local>
2020-12-28 15:29:23 -0500

    add increment decremnt buttons;

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,10 +1,12 @@
-const counter = (state = 0, action ){
-    switch(action.type){
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMETN': 
-            return state - 1;
-        default: 
-            return state;
-    }
-}
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
